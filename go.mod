fe6622c69f8e5d1bbfb36e0668e81e726ca97b2e
xuebing1110 <xuebing1110@gmail.com>
2019-11-23 15:11:29 +0800

    优化插入和查询性能

--- go.mod
+++ go.mod
@@ -1,17 +1,12 @@
 module github.com/pingfen/noticeplat-server
 
 require (
-	github.com/bingbaba/storage v0.0.0-20180611102742-da49ce2cfcd9
+	github.com/bingbaba/storage v0.0.0-20191123065946-f1f255646b95
 	github.com/chanxuehong/rand v0.0.0-20180830053958-4b3aff17f488 // indirect
 	github.com/chanxuehong/util v0.0.0-20181116100727-caf892acd09c // indirect
-	github.com/gin-contrib/sse v0.0.0-20190125020943-a7658810eb74 // indirect
-	github.com/gin-gonic/gin v1.3.0
-	github.com/golang/protobuf v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.4 // indirect
 	github.com/mozillazg/go-cos v0.11.0 // indirect
 	github.com/pkg/errors v0.8.1
-	github.com/ugorji/go/codec v0.0.0-20190204201341-e444a5086c43 // indirect
 	gopkg.in/chanxuehong/wechat.v2 v2.0.0-20180924084534-7e0579cb5377
-	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
-	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
+
+go 1.13
--- pkg/group/member.go
+++ pkg/group/member.go
@@ -3,9 +3,11 @@ package group
 import (
 	"context"
 	"fmt"
+	"github.com/bingbaba/storage"
 	pkgstore "github.com/pingfen/noticeplat-server/pkg/storage"
 	"github.com/pingfen/noticeplat-server/pkg/user"
 	"github.com/pkg/errors"
+	"strings"
 	"sync"
 )
 
@@ -79,6 +81,26 @@ func ListMembers(ctx context.Context, gid string) ([]*Member, error) {
 	return ret, nil
 }
 
+func ListMemberIds(ctx context.Context, gid string) ([]string, error) {
+	store, err := pkgstore.Get()
+	if err != nil {
+		return nil, err
+	}
+
+	key := fmt.Sprintf("/groups/%s/members", gid)
+	item_keys, err := store.List(ctx, key, &storage.SelectionPredicate{KeyOnly: true}, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "list members failed")
+	}
+
+	ret := make([]string, len(item_keys))
+	for i, k := range item_keys {
+		ret[i] = strings.TrimPrefix(k.(string), key+"/")
+	}
+
+	return ret, nil
+}
+
 func ListDetailMembers(ctx context.Context, gid string) ([]*Member, error) {
 	ms, err := ListMembers(ctx, gid)
 	if err != nil {
@@ -86,7 +108,6 @@ func ListDetailMembers(ctx context.Context, gid string) ([]*Member, error) {
 	}
 
 	var wg sync.WaitGroup
-
 	for _, m := range ms {
 		select {
 		case <-ctx.Done():
